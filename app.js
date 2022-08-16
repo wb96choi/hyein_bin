5a30a158fd12570f976aa50865cd273cc28b3c68
1_b1n <choi44601280@gmail.com>
2022-08-16 23:03:13 +0900

    푸터끝내줘

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 $(function(){
-    $('#main-header #header-nav .gnb  li').mouseenter(function(){
+    // 헤더의 네비게이션 슬라이드 코드
+    $('#main-header #header-nav .gnb li').mouseenter(function(){
         $('.sub').stop().slideDown();
         $('#main-header #header-nav .nav_bg').stop().slideDown();
     });
-    $('#main-header #header-nav .gnb > li').mouseout(function(){
+    $('#main-header #header-nav .gnb > li').mouseleave(function(){
         $('.sub').stop().slideUp();
         $('#main-header #header-nav .nav_bg').stop().slideUp();
     });
+    // --
 
+
+    // 토글메뉴버튼 코드
     $('#toggle-menu-btn .bi-list').click(function(){
         // 헤더배경색
         $('#main-header').addClass('on');
@@ -20,7 +24,7 @@ $(function(){
         $('#intranet').addClass('on');
         // 토글메뉴버튼
         $('#toggle-menu-btn').addClass('on');
-        $('.sub').stop().slideDown();
+        $('#main-header #header-nav .gnb li .sub').stop().slideDown();
         $('#main-header #header-nav .nav_bg').stop().slideDown();
         $('#toggle-menu-btn .bi-list').addClass('change');
         $('#toggle-menu-btn .bi-x-circle').removeClass('change');
@@ -38,12 +42,33 @@ $(function(){
         // 토글메뉴버튼
         $('#toggle-menu-btn').removeClass('on');
         $('#main-header').removeClass('on');
-        $('.sub').stop().slideUp();
+        $('#main-header #header-nav .gnb li .sub').stop().slideUp();
         $('#main-header #header-nav .nav_bg').stop().slideUp();
         $('#toggle-menu-btn .bi-list').removeClass('change');
         $('#toggle-menu-btn .bi-x-circle').addClass('change');
     });
+    // --
 });
+
+// 모바일 토글메뉴
+$(function(){
+    $('#toggle-menu-btn .bi-list').click(function(){
+        $('#mobile-toggle-menu').addClass('on');
+    });
+    $('#toggle-menu-btn .bi-x-circle').click(function(){
+        $('#mobile-toggle-menu').removeClass('on');
+    });
+
+    $('#mobile-toggle-menu .gnb > li > #plus').click(function(){
+        $(this).siblings('.sub').toggle(function(){
+            $(this).find('.sub').stop().slideDown(500);
+            $(this).find('.sub').stop().slideUp(500);
+        });
+    })
+});
+
+
+
 
 // 스크롤좌표값 구하기
 $(function () {
@@ -80,4 +105,23 @@ $(function () {
             $('#toggle-menu-btn').removeClass('on');
         }
     });
+});
+
+
+// 페이드 슬라이드
+$(function(){
+    var num = 0;
+    function changeslider(img_num){
+        if(num == 2){
+            num = 0;
+        }   else{
+            num++;
+        }
+        $('#main-visual .slider img').eq(num).fadeIn(500);
+        $('#main-visual .slider img').eq(num - 1).fadeOut(500);
+    };
+
+    setInterval(function(){
+        changeslider();
+    }, 3000);
 });
